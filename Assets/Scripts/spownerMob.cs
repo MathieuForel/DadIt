dd00016a995cd171804c2f829fa1bf565eff9f1d
ZircliX <lolofy71@gmail.com>
2024-01-25 17:02:01 +0100

    End Jeudi

--- Assets/Scripts/spownerMob.cs
+++ Assets/Scripts/spownerMob.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading;
-using UnityEditor.Search;
 using UnityEngine;
 
 public class random_generator : MonoBehaviour
